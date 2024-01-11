ec9af22450cd646ef62aea30e852de6bb52db690
cyber-shah <pranchx@gmail.com>
2024-01-10 19:56:37 -0500

    simpler implementation

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,17 +1,28 @@
-from flask import Flask
-from server import Server
 
+from flask import Flask, render_template
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
-# Create an instance of the Server class
-server = Server(app)
+socketio = SocketIO(app)
 
 
 @app.route('/')
-def hello_world():
-    server.run()
-    return 'Hello, World! from route /'
+def index():
+    return "Hello, this is your Flask-SocketIO server!"
+
+
+# This event will be called when the server recieves a 'message'
+# event from the client
+@socketio.on('message')
+def handle_message(msg):
+    print('Message:', msg)
+    socketio.emit('message', msg)
+
+
+@socketio.on('connect')
+def test_connect():
+    return "Client connected"
 
 
 if __name__ == '__main__':
-    app.run(debug=True)  # This will print 'Hello, World!' to the console
+    socketio.run(app, debug=True)
