300e326792d1559f46af43999815d8459f5a1ae8
cyber-shah <pranchx@gmail.com>
2024-01-10 22:52:06 -0500

    sending messages works

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,25 +5,19 @@ from flask_socketio import SocketIO
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 socketio = SocketIO(app, cors_allowed_origins='*')
-# Enable CORS for all routes
-
-
-@app.route('/')
-def index():
-    return "Hello, this is your Flask-SocketIO server!"
-
-
-# This event will be called when the server recieves a 'message'
-# event from the client
-@socketio.on('message')
-def handle_message(msg):
-    print('Message:', msg)
-    socketio.emit('message', msg)
+# Enable CORS for all routesapp
 
 
 @socketio.on('connect')
 def test_connect():
-    print('Client connected')
+    print("Client connected")
+
+
+@socketio.on('message')
+def handle_message(message):
+    data = message
+    print('received message from ' +
+          data.get('name') + ': ' + data.get('message'))
 
 
 if __name__ == '__main__':
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,51 @@
 import './App.css';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import io from 'socket.io-client';
+import { Container, Paper, TextField, Button, Typography } from '@mui/material';
+
+const socket = io('http://127.0.0.1:5000');
 
 function App() {
-  useEffect(() => {
-    const socket = io('http://127.0.0.1:5000');
+  const [messages, setMessages] = useState([]);
+  const [message, setMessage] = useState('');
+  const [name, setName] = useState('');
 
+  useEffect(() => {
     socket.on('connect', () => {
-      console.log('connected with the server');
+      console.log('connected to the server');
     })
   }, []);
 
+  const sendMessage = (message) => {
+    socket.emit('message', { name: name, message: message });
+  };
+
   return (
     <div className="App">
+      <Paper elevation={3} className="paper">
+        <Typography variant="h4" className="header">Chat App</Typography>
+        <Container className="container">
+          <TextField
+            fullWidth
+            label="Enter your name"
+            onChange={input => setName(input.target.value)}
+            variant="standard" />
+
+          <TextField
+            fullWidth
+            label="Enter your message"
+            onChange={input => setMessage(input.target.value)}
+            variant="standard" />
+
+          <Button
+            variant="contained"
+            color="primary"
+            fullWidth
+            onClick={() => sendMessage(message)}>
+            Send
+          </Button>
+        </Container>
+      </Paper>
 
     </div>
   );
