6bd7eceb5af3f34272ee871dc2f709a2e54cd3ff
cyber-shah <shahcodes@proton.me>
2024-03-18 13:02:05 -0400

    relationships

--- backend/auther/views.py
+++ backend/auther/views.py
@@ -1,4 +1,4 @@
-# NOTE: this file handles the requests and dispactches 
+# NOTE: this file handles the requests and dispactches
 # relevant functions based on them
 
 # Create your views here.
@@ -6,16 +6,15 @@
 # Models created an database object for us
 # This part modifies the database based on the request.
 
-from django.http.response import HttpResponse
-from rest_framework.compat import apply_markdown
-from rest_framework.exceptions import status
-from rest_framework.schemas.coreapi import serializers
-from .userSerializer import UserSerializer
 from rest_framework import response
 from rest_framework.decorators import api_view
-from .models import User
+from rest_framework.exceptions import status
 
-@api_view(['POST'])
+from .models import User
+from .userSerializer import UserSerializer
+
+
+@api_view(["POST"])
 def register_user(request):
     serializers = UserSerializer(data=request.data)
     if serializers.is_valid():
@@ -24,46 +23,64 @@ def register_user(request):
     else:
         return response.Response(serializers.errors, status.HTTP_400_BAD_REQUEST)
 
-@api_view(['GET'])
+
+@api_view(["GET"])
 def login_user(request):
     try:
-        user = User.objects.get(user_name = request.data.get('user_name'))
+        user = User.objects.get(user_name=request.data.get("user_name"))
     except:
-        return response.Response({'error': 'User does not exist'},status = status.HTTP_400_BAD_REQUEST)
-    
-    if user.password == request.data.get('password'):
-        return response.Response({'success': 'User validated'}, status = status.HTTP_202_ACCEPTED)
-    else:
-        return response.Response({'error': 'invalid password'}, status = status.HTTP_400_BAD_REQUEST)
+        return response.Response(
+            {"error": "User does not exist"}, status=status.HTTP_400_BAD_REQUEST
+        )
 
-@api_view(['GET'])
+    if user.password == request.data.get("password"):
+        space = request.data.get("space")
+        user.space = space
+        user.save()
+        return response.Response(
+            {"success": "User validated"}, status=status.HTTP_202_ACCEPTED
+        )
+    else:
+        return response.Response(
+            {"error": "invalid password"}, status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+@api_view(["GET"])
 def get_users(request):
     users = User.objects.all()
-    serializers = UserSerializer(users, many=True) 
+    serializers = UserSerializer(users, many=True)
     return response.Response(serializers.data)
 
-@api_view(['DELETE'])
+
+@api_view(["DELETE"])
 def delete_user(request):
     try:
         user = User.objects.get(pk=request.data.user_name)
     except:
         # NOTE :we just return a rest_framework response and then the frontend is responsible for picking
         # this up and rendering the HTML
-        return response.Response({'error': 'User does not exist'},status = status.HTTP_400_BAD_REQUEST)
-   
+        return response.Response(
+            {"error": "User does not exist"}, status=status.HTTP_400_BAD_REQUEST
+        )
+
     # once we retrieve data from the database, it becomes a python object
     # we can make all the changes to the object and hit save to send it to the database
     # so all the work is done on python object, except for retreival
     user.delete()
-    return response.Response({'success' : 'User deleted'}, status = status.HTTP_202_ACCEPTED)
+    return response.Response(
+        {"success": "User deleted"}, status=status.HTTP_202_ACCEPTED
+    )
 
 
-@api_view(['PUT'])
+@api_view(["PUT"])
 def update_user(request):
     try:
-        user = User.objects.get(user_name=request.data.get('user_name'))
+        user = User.objects.get(user_name=request.data.get("user_name"))
     except:
-        return response.Response({'error': 'User does not exist'},status = status.HTTP_400_BAD_REQUEST)
+        return response.Response(
+            {"error": "User does not exist"}, status=status.HTTP_400_BAD_REQUEST
+        )
 
     # to update, we pass in the user, and then the data of that user to be updated
     # if we don't specify partial_update TRUE here, it will need all the fields and when it checks
@@ -71,7 +88,8 @@ def update_user(request):
     serialzed_user = UserSerializer(user, data=request.data, partial=True)
     if serialzed_user.is_valid():
         serialzed_user.save()
-        return response.Response({'success': 'User data updated'}, status = status.HTTP_202_ACCEPTED)
+        return response.Response(
+            {"success": "User data updated"}, status=status.HTTP_202_ACCEPTED
+        )
     else:
-        return response.Response(status= status.HTTP_400_BAD_REQUEST)
-
+        return response.Response(status=status.HTTP_400_BAD_REQUEST)
--- backend/spaces/models.py
+++ backend/spaces/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from auther.models import User
 # Create your models here.
+
+
+class Spaces(models.Model):
+    name = models.CharField(max_length=1000)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    creator = models.ForeignKey(User, on_delete=models.DO_NOTHING)
